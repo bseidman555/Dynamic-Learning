a4cd793a91516d0acf9ee077cbf86a65767e121c
Brad Seidman <bseidman555@gmail.com>
2020-02-17 19:19:17 -0500

    Add http post and http patch to static api

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -9,21 +9,45 @@ import {HttpClient} from '@angular/common/http';
 export class PostsComponent implements OnInit {
 
   readonly ROOT_URL = 'https://jsonplaceholder.typicode.com'
-
-  posts: any
+  
+  posts: any ;
+  responseObj: any;
   constructor( private http: HttpClient) { }
     
     getPosts(){
-      this.posts= this.http.get(this.ROOT_URL + '/posts').subscribe(response =>{
-        console.log('response');
+      this.http.get(this.ROOT_URL + '/posts')
+      .subscribe(response =>{
+        console.log(response);
+        this.posts = response;
       });
-      console.log(this.posts)
-      
+    }
+    createPosts(input: HTMLInputElement ) {
+      let post={title: input.value}
+      input.value = '';
+      this.http.post(this.ROOT_URL + '/posts', JSON.stringify(post))
+      .subscribe(response=>{
+        this.responseObj = response;
+        this.posts.splice(0,0, post)
+        console.log("yayyyy id" , this.responseObj.id);
+        console.log(response)
+        
+
+      })
     }
 
+    updatePosts(post){
+      
+      this.http.put(this.ROOT_URL + '/posts/' + post.id, JSON.stringify({isRead: true}))
+      .subscribe(response =>{
+        this.responseObj = response;
+        console.log(response);
+
+      })
+      //this.http.put(this.ROOT_URL, JSON.stringify({post}))
+
+    }
     ngOnInit() {
     
     }
-
   }
   
